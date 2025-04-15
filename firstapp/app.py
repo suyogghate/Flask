f61b79289e9c05e25d5757c741852f2ba2eb7d44
suyogghate <suyogghate96@gmail.com>
2025-04-15 23:37:34 +0530

    creating first flask project with basic routes

---
+++ firstapp/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Hello World</h1>"
+
+@app.route('/hello')
+def hello():
+    return "Hello World"
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello {name}"
+
+@app.route('/add/<int:number1>/<int:number2>')
+def add(number1, number2):
+    return f"{number1} + {number2} = {number1 + number2}"
+
+@app.route('/handle_url_params')
+def handle_params():
+    return str(request.args)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
