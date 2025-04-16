21402f2ca70678bc394bb567be07f35ee71d734c
suyogghate <suyogghate96@gmail.com>
2025-04-16 23:14:58 +0530

    handle url params, GET,POST request, status codes,make response

--- firstapp/app.py
+++ firstapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
@@ -6,9 +6,22 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello World</h1>"
 
+# @app.route('/hello', methods=['GET', 'POST'])
 @app.route('/hello')
 def hello():
-    return "Hello World"
+    # if request.method == 'GET':
+    #     return 'You made a GET request\n'
+    # elif request.method == 'POST':
+    #     return 'You made a POST request\n'
+    # else:
+    #     return "Hello World\n"
+    
+    # return 'Hello World', 404
+
+    response = make_response('Hello World\n')
+    response.status_code = 202
+    response.headers['content-type'] = 'text/plain'
+    return response
 
 @app.route('/greet/<name>')
 def greet(name):
@@ -20,7 +33,13 @@ def add(number1, number2):
 
 @app.route('/handle_url_params')
 def handle_params():
-    return str(request.args)
+    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
+        # greeting = request.args.get('greeting')
+        greeting = request.args['greeting']
+        name = request.args.get('name')
+        return f"{greeting}, {name}"
+    else:
+        return "Some parameters are missing"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
