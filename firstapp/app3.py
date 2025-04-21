fb1d885f9b2ab03504fbb1afa72b8d4517e8a5ad
suyogghate <suyogghate96@gmail.com>
2025-04-21 23:50:26 +0530

    file upload in flask route

--- firstapp/app3.py
+++ firstapp/app3.py
@@ -15,5 +15,20 @@ def index():
         else:
             return "Failure"
         
+@app.route('/file_upload', methods=['POST'])
+def file_upload():
+    file = request.files['file']
+
+    if file.content_type == 'text/plain':
+        return file.read().decode()
+    # elif file.content_type == '.doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+    #     df = pd.read_excel(file)
+    #     return df.to_html()
+
+@app.route('/convert_csv')
+def convert_csv():
+    return ""
+
+        
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
