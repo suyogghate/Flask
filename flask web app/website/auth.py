40ffac22012927c7c521bd95a2ab16a2eb3d7676
suyogghate <suyogghate96@gmail.com>
2025-04-28 23:38:36 +0530

    adding validation in sign up route and flashing error/success messages.

--- flask web app/website/auth.py
+++ flask web app/website/auth.py
@@ -1,11 +1,9 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
     return render_template("login.html", boolean=False)
 
 @auth.route('/logout')
@@ -14,5 +12,22 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be atleast 7 characters.', category='error')
+        else:
+            flash('Account created successfully!', category='success')
+
     return render_template("sign_up.html")
 
